0a57e8148d55471305b2f0d691594ea0331786b1
vladbakhteev <bakhteev_v@mail.ru>
2021-04-18 16:25:06 +0300

    Fix client features

--- utils/features.py
+++ utils/features.py
@@ -27,20 +27,13 @@ def get_month_payments(df_funnel, df_payments) -> pd.DataFrame:
 
 def get_funnel_features(funel) -> pd.DataFrame:
     funel['client_segment'] = funel['client_segment'].astype(int)
-    funel['region_cd'] = funel['region_cd'].fillna(0)
-    funel['region_cd'] = funel['region_cd'].astype(int)
+    funel['region_cd'] = funel['region_cd'].fillna(-1).astype(int)
 
-    funel['feature_7'] = funel['feature_7'].fillna(3642369.0)
+    funel['feature_7'] = funel['feature_7'].fillna(-1)
     funel['feature_10div7'] = funel.feature_10 / funel.feature_7
-    funel['feature_7'] = np.sqrt(funel['feature_7'])
-
-    funel['feature_8'] = funel['feature_8'].fillna(3314257.0)
-
-    funel['feature_9'] = funel['feature_9'].fillna(7.9)
-    funel['feature_9'] = (funel['feature_9'] * 10).astype(int)
-
-    funel['feature_10'] = funel['feature_10'].fillna(48149.0)
-    funel['feature_10'] = np.log1p(funel['feature_10'])
+    funel['feature_8'] = funel['feature_8'].fillna(-1)
+    funel['feature_9'] = funel['feature_9'].fillna(-1)
+    funel['feature_10'] = funel['feature_10'].fillna(-1)
 
     funel['freq_client_segment'] = funel.groupby('client_segment')['client_segment'].transform('count')
     funel['freq_feature_1'] = funel.groupby('feature_1')['feature_1'].transform('count')
@@ -49,13 +42,27 @@ def get_funnel_features(funel) -> pd.DataFrame:
     funel['freq_feature_1_client_segment'] = funel.groupby('feature_1_client_segment')['feature_1_client_segment'].transform('count')
     return funel
 
+def get_aum_features(funnel, aum):
+    funnel = funnel.set_index('client_id')
+    gb = aum.groupby('client_id')['balance_rur_amt']
+    mean_balance = gb.mean()
+    std_balance = gb.std()
+
+    funnel['mean_balance'] = 0
+    funnel['std_balance'] = 0
+    funnel.loc[mean_balance.index, 'mean_balance'] = mean_balance
+    funnel.loc[std_balance.index, 'std_balance'] = std_balance
+
+    return funnel.reset_index()
+
 
 def get_client_features(funel, client) -> pd.DataFrame:
-    client['gender'] = client['gender'].fillna('M').map(lambda x: int(hashlib.sha1(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8))
+    client['gender'] = client['gender'].fillna('UNK')
     client['age'] = client['age'].fillna(client['age'].median()).astype(int)
-    client['citizenship'] = client['citizenship'].fillna('RUSSIA').map(lambda x: int(hashlib.sha1(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8))
-    client['education'] = client['education'].fillna('HIGHER_PROFESSIONAL').map(lambda x: int(hashlib.sha1(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8))
-    client['job_type'] = client['job_type'].fillna("USELESS_JOB").map(lambda x: int(hashlib.sha1(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8))
+    client['education'] = client['education'].fillna('UNK')
+    client['job_type'] = client['job_type'].fillna("UNK")
+    client = client[['client_id', 'gender', 'age', 'education', 'job_type', 'region', 'city']]
+
     funel = funel.merge(client, on='client_id', how='left')
     return funel
 
